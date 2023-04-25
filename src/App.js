f0f381922efbfbaefc79d866a4c361cb30f64dde
JungHoeIn <hizz666@naver.com>
2023-04-25 13:59:14 +0900

    'finish'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
-import Home from "./routes/Home";
 import './App.css';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import About from './routes/About';
+import Home from "./routes/Home";
+import Nav from "./component/Nav";
+import Detail from "./routes/Detail";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+       <Nav />
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/about" Component={About} />
+        <Route path="/movie-detail" Component={Detail} />
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Detail() {
+    return <p>Detail Page</p>;
+}
+
+
+
+export default Detail;
