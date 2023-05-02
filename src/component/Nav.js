6f07d680ba73f131a5c326d6955188547d44a601
JungHoeIn <hizz666@naver.com>
2023-05-02 09:48:32 +0900

    movie

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
     return (
         <div className="nav">
             <Link to="/">Home</Link>
-            <Link to="/지선">안지선은 보아라.</Link>
+            <Link to="/지선">About</Link>
         </div>
     );
 }
